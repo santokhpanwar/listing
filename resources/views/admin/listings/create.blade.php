26f1ffdabb7c67be903256d0ac5d38b1d4dfacb7
Santokh Singh <santokhpanwar@gmail.com>
2023-09-14 06:34:55 +0530

    Added Iamge function

--- resources/views/admin/listings/create.blade.php
+++ resources/views/admin/listings/create.blade.php
@@ -4,46 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-<<<<<<< HEAD
-            <div class="card mb-3">
-                <div class="card-header">{{ __('Create listing') }}</div>
-                <div class="card-body">
-                    <h1 class="my-4">Add New</h1>
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
-                    <form action="/admin/listings" method="post">
-                        @csrf
-                        <div class="form-group mb-3">
-                            <label for="">listing Title</label>
-                            <input type="text" name="title" class="form-control">
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <label for="">listing Body</label>
-                            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <label for="">Publish At</label>
-                            <input type="date" name="published_at" class="form-control">
-                        </div>
-
-                        <input class="d-block mx-auto" type="file" id="imageInput" accept="image/*"  name="images[]" multiple>
-                        
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                    
-                </div>
-                
-            </div>
-
-
-=======
->>>>>>> 29a9bd554352b0511fa9ec5fc445536932103033
             <div class="card">
                 <form action="/admin/listings" method="post">
                     @csrf  
@@ -58,7 +18,6 @@
                                         {{ session('status') }}
                                     </div>
                                 @endif
-                                <!-- @php print_r ($errors); @endphp -->
                                 <form action="/admin/listings" method="post">
                                     @csrf                        
                                     <div class="form-group mb-3 {{ $errors->has('title') ? 'has-error' : '' }}"> 
